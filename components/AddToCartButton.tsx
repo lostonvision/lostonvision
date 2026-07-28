b3c48387120521238486247d7b01f999930ab1e8
LOSTONVISION <lostonvision@gmail.com>
2026-07-28 22:30:37 +0200

    Fix cart price type for production build

--- components/AddToCartButton.tsx
+++ components/AddToCartButton.tsx
@@ -18,7 +18,10 @@ export default function AddToCartButton({ product }: Props) {
         <button
             onClick={() => {
                 console.log("Añadido al carrito");
-                addToCart(product);
+                addToCart({
+                    ...product,
+                    price: Number(product.price),
+                });
             }}
             className="mt-10 bg-black text-white py-4 rounded-lg hover:opacity-90 transition w-full"
         >
